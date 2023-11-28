0b02699bc1a503f1c13242206d5fb7aac7d32231
magalicpujol <magalicpujol@gmail.com>
2023-11-28 15:11:09 -0300

    agrega delete y post
    Signed-off-by: magalicpujol <magalicpujol@gmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
+
 var app = express();
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
-app.get("/", (req, res, next) => {
-    res.json(["Peco", "Peke", "Pecos", "Quepe", "Quepos"]);
+var names = ["Peco", "Peke", "Pecos", "Quepe", "Quepos"];
+
+app.get("/names", (req, res, next) => {
+    res.json(names);
+});
+
+app.delete("/names/:name", (req, res, next) => {
+    const nameToDelete = req.params.name;
+    const index = names.indexOf(nameToDelete);
+    if (index !== -1) {
+        names.splice(index, 1);
+        res.json({ message: "Name deleted successfully", names });
+    } else {
+        res.status(404).json({ message: "Name not found" });
+    }
+});
+
+app.post("/names", (req, res, next) => {
+    const newName = req.body.name;
+    if (newName) {
+        names.push(newName);
+        res.json({ message: "Name added successfully", names });
+    } else {
+        res.status(400).json({ message: "Invalid request. Please provide a name in the request body." });
+    }
 });
